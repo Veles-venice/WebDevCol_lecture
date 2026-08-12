e3aff9e5a0ccd962f15654d63829669afb453c3e
Veles-venice <veniceveles@gmail.com>
2026-08-12 11:47:48 +0530

    A bit of study using postman and requests

--- class4/index.js
+++ class4/index.js
@@ -18,29 +18,48 @@ const packages = require('./module/pacg.js');
 console.log(packages);
 */
 //A bit of data
+
+
+
+const http = require('http');
+
 const students = [
 
 ];
 
 
-const http = require('http');
-
-require('dotenv').config()
-
-const process = require('process');
-//console.log(process.argv)
-//console.log(process.env.PORT);
-const port = process.env.PORT || 3000;
 
 
 const server = http.createServer((req, res)=>{
+    
     if(req.method === 'GET' && req.usl === '/api/students'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(
             JSON.stringify({count: students.length, students})
         )
     }
+    else if (req.method === 'GET' && req.url === '/api/students/count'){
+        res.writeHead(200, {'Content-Type' : 'applicaiton/json'});
+        res.end(JSON.stringify({count: students.length}));
+    }
+    else if (req.method === 'GET' && req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'}),
+        res.write('<h1>hello world</h1>'),
+        res.write('<p>This is my first server</p>'),
+        res.write('<p>My name is Krish</p>'),
+        res.end()
+    }
 });
+
+
+
+// server starting 
+require('dotenv').config()
+
+const process = require('process');
+//console.log(process.argv)
+//console.log(process.env.PORT);
+const port = process.env.PORT || 3000;
 
 server.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
