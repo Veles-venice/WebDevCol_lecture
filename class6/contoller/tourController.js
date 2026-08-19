1c83e9f71e9c9171b66eb334900b341988b99c5c
Veles-venice <veniceveles@gmail.com>
2026-08-19 12:41:13 +0530

    end of boring class

--- class6/contoller/tourController.js
+++ class6/contoller/tourController.js
@@ -1,3 +1,22 @@
+const tourModel = require("../model/tourModel");
+
+//get all tours
+const getAllTours = (req, res) => {
+    const tours = tourModel.getAll();
+    res.json(tours)
+};
+
+module.exports = {
+    getAllTours
+}
+
+
+
+
+
+
+
+
 const packages = require("./data/tour")
 app.get("/", (req, res) => {
     res.status(200).res.send("Hello, World");
